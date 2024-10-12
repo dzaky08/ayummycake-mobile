fe53cf94fe5c40c72282081800ba7f413cddda75
dzaky 09 <dzaky08@gmail.com>
2024-10-12 13:02:34 +0700

    menambahkan introduction screen

--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import "package:ayummycakeshop/component/ev_color.dart";
+import "package:ayummycakeshop/screens/Introduction/introduction.dart";
+import "package:flutter/material.dart";
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -10,7 +11,32 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    super.initState();
+    startTimer();
+  }
+
+  startTimer() async {
+    await Future.delayed(
+        const Duration(seconds: 3),
+        () => Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+              builder: (_) => const IntroductionScreen(),
+            )));
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: EVColor.primary,
+      body: Center(
+        child: Image.asset(
+          "assets/images/logoayummy1.png",
+          height: 300,
+          width: 300,
+        ),
+      ),
+    );
   }
 }
